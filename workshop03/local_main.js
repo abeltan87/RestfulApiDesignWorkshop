7dc2076a5e60f96e7dd1d9748835a73abc4f0602
Abel Tan <abeltan.87s@gmail.com>
2021-10-01 14:21:26 +0800

    Update to local_main

--- workshop03/local_main.js
+++ workshop03/local_main.js
@@ -28,33 +28,32 @@ app.use(express.urlencoded({ extended: true }));
 
 // Start of workshop
 
-// TODO 1/2 Load schemans
+//Load application keys
+const db = CitiesDB(data);
 
+const app = express();
 
-TODO - load openapi here
-.install(app)
-.then(() => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 	
-	// Start of workshop
-	// TODO 2/2 Copy your routes from workshop02 here
+// Start of workshop
+// TODO 2/2 Copy your routes from workshop02 here
 
-	// End of workshop
+// End of workshop
 
-	app.use('/schema', express.static(join(__dirname, 'schema')));
+app.use('/schema', express.static(join(__dirname, 'schema')));
 
-	app.use((error, req, resp, next) => {
+app.use((error, req, resp, next) => {
 
-		console.error('Error: ', error)
+	console.error('Error: ', error)
 
-		return resp.status(error.status || 500)
-			.type('application/json').json({ error: error });
+	return resp.status(error.status || 500)
+		.type('application/json').json({ error: error });
 
-	});
+});
 
-	const PORT = parseInt(process.argv[2] || process.env.APP_PORT) || 3000;
-	app.listen(PORT, () => {
-		 console.info(`Application started on port ${PORT} at ${new Date()}`);
-	});
-
-})
-
+const PORT = parseInt(process.argv[2] || process.env.APP_PORT) || 3000;
+app.listen(PORT, () => {
+		console.info(`Application started on port ${PORT} at ${new Date()}`);
+});
